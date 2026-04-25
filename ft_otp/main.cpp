f07906bb2fc6c622eb90dcde20aa852283eb50ca
Achraf Toukmati <atoukmat@c1r8p3.1337.ma>
2026-04-25 15:48:09 +0100

    cpp

---
+++ ft_otp/main.cpp
@@ -0,0 +1,18 @@
+
+#include <string>
+#include <iostream>
+#include <exception>
+#include <stack>
+#include <algorithm>
+#include <sstream>
+
+
+int main(int ac , char **av){
+
+    if(ac <= 1)
+        std::cout << "Zeb" << std::endl;
+        return 0;
+
+
+
+}
